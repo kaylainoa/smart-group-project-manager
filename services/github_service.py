422f7b6cd6dbc9d8ee9348d94f1479a7461e8ae6
Biftu Mohammed <biftumohammed22@gmail.com>
2026-07-07 16:05:58 +0000

    Start GitHub API service

---
+++ services/github_service.py
@@ -0,0 +1,9 @@
+# This file handles GitHub API requests for the project.
+#  connect our Flask app to GitHub project data.
+
+import requests
+from config import GITHUB_TOKEN
+
+
+OWNER = "kaylainoa"
+REPO = "smart-group-project-manager"
