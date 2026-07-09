44f1b8bde32b81256791d29594f8084cf588ca28
Jakha Cham <you@example.com>
2026-07-09 12:15:34 -0400

    Fixed port issue

--- app.py
+++ app.py
@@ -359,4 +359,4 @@ def reports():
 
 if __name__ == "__main__":
     # app.run(debug=True, port=8080)
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
